55b841c339699534ffa8a48ae46768b379115dc0
Justin Willis <jgw9617@gmail.com>
2015-12-03 22:54:53 -0600

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 3700;
+var port = 8080;
 
 app.use(express.static(__dirname + '/public'));
 app.use("/modules/", express.static(__dirname + "/node_modules"))
