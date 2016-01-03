8e4a0a11694a2c175c49fdf2c7c97b90a27a1fd4
Justin Willis <jgw9617@gmail.com>
2016-01-03 00:20:22 -0600

    new loading spinner

--- public/app/components/login/login-component.ts
+++ public/app/components/login/login-component.ts
@@ -3,35 +3,35 @@ import { Router, RouterLink } from 'angular2/router';
 import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 @Component({
-	selector: "login",
-	templateUrl: 'app/components/login/login-component.html',
-	directives: [ROUTER_DIRECTIVES, RouterLink]
+    selector: "login",
+    templateUrl: 'app/components/login/login-component.html',
+    directives: [ROUTER_DIRECTIVES, RouterLink]
 })
 export class LoginComponent {
-	public user: string;
-	public saved: boolean;
+    public user: string;
+    public saved: boolean;
 
-	constructor() {
-		let clickEvent = new MouseEvent("click", {
-			"view": window,
-			"bubbles": true,
-			"cancelable": false
-		});
-		
-		this.saved = false;
+    constructor() {
+        let clickEvent = new MouseEvent("click", {
+            "view": window,
+            "bubbles": true,
+            "cancelable": false
+        });
 
-		if (localStorage.getItem("username") !== null) {
-			document.querySelector("#loginButton").dispatchEvent(clickEvent);
-			console.log("clicked");
-		}
-	}
+        this.saved = false;
 
-	login(name: string) {
-		localStorage.setItem("username", name);
-		document.querySelector("#saveButton").classList.add("fadeOutLeft");
-		document.querySelector("#inputBox").classList.add("fadeOutLeft");
-		setTimeout(() => {
-			this.saved = true;
-		},700)
-	}
+        if (localStorage.getItem("username") !== null) {
+            document.querySelector("#loginButton").dispatchEvent(clickEvent);
+            console.log("clicked");
+        }
+    }
+
+    login(name: string) {
+        localStorage.setItem("username", name);
+        document.querySelector("#saveButton").classList.add("fadeOutLeft");
+        document.querySelector("#inputBox").classList.add("fadeOutLeft");
+        setTimeout(() => {
+            this.saved = true;
+        }, 700)
+    }
 }
